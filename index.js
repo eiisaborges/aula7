ba32613f1f17f9053cc6583f5a54fa8441f4c59f
eiisaborges <142436607+eiisaborges@users.noreply.github.com>
2023-08-23 17:14:04 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,95 @@
+/*let a = 3;
+let b = 5;
+let c = 6;
+let d = 7;
+let e = 4;
+
+console.log(a % 2);
+console.log(b % 2);
+console.log(c % 2);
+console.log(d % 2);
+console.log(e % 2);
+*/
+
+//idade
+
+/*let minhaIdade = 26;
+console.log("Antes", minhaIdade);
+minhaIdade = minhaIdade + 1
+console.log("Depois", minhaIdade);
+
+let soma = 3 + 4;
+console.log(3+4);
+let multiplique = 3 * 5;
+console.log(3*5);
+console.log(multiplique /2);
+let subtrair = 5 - 4;
+console.log(5-4);
+console.log(subtrair * -1);
+let divisão = 234 / 5;
+console.log(divisão % 5);
+ 
+
+let C = 3;
+let D = 5;
+let E = 2;
+let F = -1;
+console.log(c * d /2);
+console.log(d - b *F); 
+let G = 234;
+let H = 5;
+console.log(G % H);
+*/
+//Exercício2
+/*let a = 8;
+let b = 4; 
+console.log(a === b);
+console.log(a !== b);
+console.log(a > b);
+console.log(a < b);
+
+let A = 5; 
+let B = 3;
+let C = 0;
+
+console.log(A === B || A !== C);
+console.log(A === B && A !== C);
+console.log("---- separador ----");
+
+console.log(true || false);
+console.log(true && false);
+console.log(true && true);
+*/
+//Exercício3
+/*let a = true;
+let b = false;
+let c = true;
+console.log( a && b);
+console.log( b && c);
+console.log( a && c);
+console.log( a && b && c);
+
+console.log( a || b);
+console.log( b || c);
+console.log( a || c);
+console.log( a || b || c);
+*/
+
+//negacao
+
+let a = true;
+let b = false;
+
+console.log(a);
+console.log(!a);
+
+console.log(b);
+console.log(!b);
+
+//Exercício5
+let nome = "Laís";
+let idade = 23;
+console.log("Nome:", nome);
+console.log("Idade:", Number (idade));
+console.log("É maior de idade?", idade > 17);
+console.log("idade em 2050?", 2050 - 1998);
